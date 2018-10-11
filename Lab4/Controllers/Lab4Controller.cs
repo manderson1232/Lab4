764ae114e95012631cf18b6172d05ac18e81fe2d
Marissa K Anderson <ander188@go.stockton.edu>
2018-10-10 23:42:09 -0400

    Add project files.

---
+++ Lab4/Controllers/Lab4Controller.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WaffleGenerator;
+
+
+namespace Lab4.Controllers
+{
+    public class Lab4Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Greeting"] = GetGreeting();
+            return View("Index");
+        }
+        public IActionResult Waffles()
+        {
+            ViewData["Greeting"] = GetGreeting();
+            return View("Waffles");
+        }
+        public string GetGreeting()
+        {
+            if (DateTime.Now.Hour < 12)
+            {
+                return "Good Morning";
+            }
+            else if (DateTime.Now.Hour < 18)
+            {
+                return "Good Afternoon";
+            }
+            else
+            {
+                return "Good Evening";
+            }
+
+        }
+
+    }
+}
